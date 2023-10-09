d81271cdcecc00e1057285900817bf189b475c2d
Alexis Zueras <alexis.zueras.pro@gmail.com>
2023-10-09 10:15:15 +0200

    update test

--- test/items_test.dart
+++ test/items_test.dart
@@ -12,7 +12,7 @@ void main() {
         'itemImage': 'http://example.com/image.jpg',
         'sellerName': 'John Doe',
         'sellerPhone': '1234567890',
-        'itemPrice': 150.0,
+        'itemPrice': "150.0",
         'createdAt': '2023-10-03T00:00:00Z',
         'status': 'Available'
       };
@@ -24,7 +24,7 @@ void main() {
       expect(item.itemImage, 'http://example.com/image.jpg');
       expect(item.sellerName, 'John Doe');
       expect(item.sellerPhone, '1234567890');
-      expect(item.itemPrice, 150.0);
+      expect(item.itemPrice, "150.0");
       // La vérification pour createdAt pourrait nécessiter une conversion de la date.
       expect(item.status, 'Available');
     });
@@ -46,7 +46,7 @@ void main() {
       expect(item.itemImage, '');
       expect(item.sellerName, '');
       expect(item.sellerPhone, '');
-      expect(item.itemPrice, 0.0);
+      expect(item.itemPrice, "0.0");
       expect(item.createdAt, '');
       expect(item.status, '');
     });
