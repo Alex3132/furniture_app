1c1364c055064917001b43cddb3c0a67e40ef8ee
Alexis Zueras <alexis.zueras.pro@gmail.com>
2023-10-10 10:08:09 +0200

    API rest complete

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,63 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const db = admin.firestore();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// CREATE: Add a new item
+exports.createItem = functions.https.onRequest(async (req, res) => {
+  try {
+    const item = {
+      itemName: req.body.itemName,
+      itemDescription: req.body.itemDescription,
+      itemImage: req.body.itemImage,
+      sellerName: req.body.sellerName,
+      sellerPhone: req.body.sellerPhone,
+      itemPrice: req.body.itemPrice,
+      status: req.body.status,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    };
+    const docRef = await db.collection('items').add(item);
+    res.status(201).send({ itemID: docRef.id });
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+// READ: Récupérer tous les éléments
+exports.getItems = functions.https.onRequest(async (req, res) => {
+  try {
+    const snapshot = await db.collection('items').get();
+    const items = [];
+    snapshot.forEach(doc => {
+      items.push({ id: doc.id, data: doc.data() });
+    });
+    res.status(200).send(items);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+// UPDATE: Mettre à jour un élément par ID
+exports.updateItem = functions.https.onRequest(async (req, res) => {
+  try {
+    const itemId = req.params.id;
+    const item = req.body;
+    await db.collection('items').doc(itemId).update(item);
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+// DELETE: Supprimer un élément par ID
+exports.deleteItem = functions.https.onRequest(async (req, res) => {
+  try {
+    const itemId = req.params.id;
+    await db.collection('items').doc(itemId).delete();
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
