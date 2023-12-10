f065ea9fdb81eda37750c538a9fc02a5e5f85682
Alexis Zueras <alexis.zueras.pro@gmail.com>
2023-12-10 18:14:05 +0100

    API 1.1.2

--- functions/index.js
+++ functions/index.js
@@ -54,11 +54,12 @@ exports.updateItem = functions.https.onRequest(async (req, res) => {
 // DELETE: Supprimer un élément par ID
 exports.deleteItem = functions.https.onRequest(async (req, res) => {
   try {
-    const itemID = req.params.id;// ou req.params.id, en fonction de la structure de l'URL
-    await db.collection("items").doc(itemID).delete();
+    const itemId = req.query.id; // ou req.params.id
+    console.log("Item ID:", itemId); // Ajouter cette ligne pour le debug
+    await db.collection("items").doc(itemId).delete();
     res.status(200).send("Item deleted successfully");
   } catch (e) {
+    console.error("Error:", e);
     res.status(400).send(e.message);
   }
 });
-
