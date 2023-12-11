45f01a846caf8c464e07b2af1ab2cc9a735fe21c
Alexis Zueras <alexis.zueras.pro@gmail.com>
2023-12-11 11:56:27 +0100

    test on delete items #3

--- functions/index.js
+++ functions/index.js
@@ -1,79 +1,67 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
 
-// CREATE: Add a new item
+// CREATE: Ajouter un nouvel item
 exports.createItem = functions.https.onRequest(async (req, res) => {
   try {
-    const item = {
+    const newItem = {
       itemName: req.body.itemName,
       itemDescription: req.body.itemDescription,
       itemImage: req.body.itemImage,
       sellerName: req.body.sellerName,
       sellerPhone: req.body.sellerPhone,
       itemPrice: req.body.itemPrice,
+      stock: req.body.stock,
       status: req.body.status,
       createdAt: admin.firestore.FieldValue.serverTimestamp(),
     };
-    const docRef = await db.collection("items").add(item);
-    res.status(201).send({itemID: docRef.id});
+    const docRef = await db.collection("items").add(newItem);
+    res.status(201).send({ itemID: docRef.id });
   } catch (e) {
+    console.error(e);
     res.status(400).send(e.message);
   }
 });
 
-
-// READ: Récupérer tous les éléments
+// READ: Récupérer tous les items
 exports.getItems = functions.https.onRequest(async (req, res) => {
   try {
-    const snapshot = await db.collection("items").get();
+    const itemsSnapshot = await db.collection("items").get();
     const items = [];
-    snapshot.forEach((doc) => {
-      items.push({id: doc.id, data: doc.data()});
+    itemsSnapshot.forEach(doc => {
+      items.push({ id: doc.id, ...doc.data() });
     });
     res.status(200).send(items);
   } catch (e) {
+    console.error(e);
     res.status(400).send(e.message);
   }
 });
 
-// UPDATE: Mettre à jour un élément par ID
+// UPDATE: Mettre à jour un item par ID
 exports.updateItem = functions.https.onRequest(async (req, res) => {
   try {
-    const itemId = req.params.id;
-    const item = req.body;
-    await db.collection("items").doc(itemId).update(item);
-    res.status(200).send();
+    const itemId = req.query.id; // Utilisez req.params.id si l'ID est dans le chemin
+    const itemToUpdate = req.body;
+    await db.collection("items").doc(itemId).update(itemToUpdate);
+    res.status(200).send(`Item with ID: ${itemId} updated successfully.`);
   } catch (e) {
+    console.error(e);
     res.status(400).send(e.message);
   }
 });
 
-// DELETE: Supprimer un élément par ID
-/*exports.deleteItem = functions.https.onRequest(async (req, res) => {
-  try {
-    const itemId = req.params.id; // ou req.params.id
-    await db.collection("items").doc(itemId).delete();
-    res.status(200).send("Item deleted successfully");
-  } catch (e) {
-    console.error("Error:", e);
-    res.status(400).send(e.message);
-  }
-});*/
+// DELETE: Supprimer un item par ID
 exports.deleteItem = functions.https.onRequest(async (req, res) => {
   try {
-    console.log("Request received with query:", req.query);
-    const itemId = req.query.id;
-    console.log("Deleting item with ID:", itemId);
-
+    const itemId = req.query.id; // Utilisez req.params.id si l'ID est dans le chemin
     await db.collection("items").doc(itemId).delete();
-    res.status(200).send("Item deleted successfully");
-    // Reste du code...
+    res.status(200).send(`Item with ID: ${itemId} deleted successfully.`);
   } catch (e) {
-    console.error("Error:", e);
+    console.error(e);
     res.status(400).send(e.message);
   }
 });
-
